efc8fa8c51bb80f50657b9d06bc2110e39668dbf
Unili <85914251+CuNO3@users.noreply.github.com>
2023-02-02 07:35:04 +0000

    Processing with Docusaurus configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'OpenLaunch',
+  tagline: 'A part of Minecraft ecosystem',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://ohmc.unili.eu.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'OpenLaunchMC', // Usually your GitHub org/user name.
+  projectName: 'ohmc-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -39,17 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -64,21 +56,51 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'OpenLaunch',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'OpenLaunch Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'intro/intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Introduction',
+          },
+          {
+            type: 'doc',
+            docId: 'crebit/intro',
+            position: 'left',
+            label: 'Crebit',
+          },
+          {
+            type: 'doc',
+            docId: 'fraxinus/intro',
+            position: 'left',
+            label: 'Fraxinus',
+          },
+          {
+            type: 'doc',
+            docId: 'ragnarok/intro',
+            position: 'left',
+            label: 'Ragnarok',
+          },
+          {
+            type: 'doc',
+            docId: 'modhashlib/intro',
+            position: 'left',
+            label: 'ModHashlib',
+          },
+          {
+            type: 'doc',
+            docId: 'jar-rs/intro',
+            position: 'left',
+            label: 'JAR-RS',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/OpenLaunchMC',
             label: 'GitHub',
             position: 'right',
           },
@@ -88,32 +110,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
@@ -122,12 +118,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/OpenLaunchMC',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} OpenLaunchMC. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
